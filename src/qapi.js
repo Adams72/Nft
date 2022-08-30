e74067c95645dcf240d48a376677343477c85058
Adams72 <99621680+Adams72@users.noreply.github.com>
2022-08-30 02:17:21 +0100

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"630d6334f913c500201d1c44",
+				"630d6334f913c500201d1c47"
+			]
+		},
+		"630d6334f913c500201d1c44": {
+			"id": "630d6334f913c500201d1c44",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"630d6334f913c500201d1c47": {
+			"id": "630d6334f913c500201d1c47",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
